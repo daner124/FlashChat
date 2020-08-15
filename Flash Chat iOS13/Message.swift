5cadafc0eb835de87069c5c0661b2d0ad74cd061
Dane Gledhill <dane.gled@gmail.com>
2020-08-15 10:32:18 -0400

    Initial commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,16 @@
+//  FROM UDEMY COURSE: SKELETON CLONED FROM GITHUB AND FILLED IN FOLLOWING COURSE
+//
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Dane Gledhill on 2020-07-20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,33 @@
+//  FROM UDEMY COURSE: SKELETON CLONED FROM GITHUB AND FILLED IN FOLLOWING COURSE
+//
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Dane Gledhill on 2020-07-20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
